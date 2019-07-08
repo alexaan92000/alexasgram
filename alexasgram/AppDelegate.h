8c5e266e289008b7f8da01369cd68a178ddf823f
Alexa Morales <alexamorales@fb.com>
2019-07-08 15:31:01 -0700

    Initial Commit

---
+++ alexasgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alexasgram
+//
+//  Created by alexamorales on 7/8/19.
+//  Copyright © 2019 alexamorales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alexasgram/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  alexasgram
+//
+//  Created by alexamorales on 7/8/19.
+//  Copyright © 2019 alexamorales. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
